7f090877f8157512ce50d04ce8115548d7acc7e2
Nikshit <kingjoshi27032@gmail.com>
2026-06-21 19:02:19 +0530

    Changes in file structure!

--- app.js
+++
@@ -1,4 +0,0 @@
-import {add} from './add.js'
-import sub from './sub.js'
-console.log(add(3,4))
-console.log(sub(10,2))
--- sub.js
+++
@@ -1,7 +0,0 @@
-function sub(a, b)
-{
-    return a - b
-}
-sub(10, 5)
-
-export default sub
